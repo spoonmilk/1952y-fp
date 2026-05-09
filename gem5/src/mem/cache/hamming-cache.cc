781209b24e83cc503b86fcd387e3b2e50330a123
patback0 <patrick_ortiz@brown.edu>
2026-05-09 22:52:59 +0000

    feat. debugging broken gem5 build

--- gem5/src/mem/cache/hamming-cache.cc
+++ gem5/src/mem/cache/hamming-cache.cc
@@ -316,7 +316,7 @@ void HammingCache::relaxScrubInterval() {
     newInterval = scrubIntervalCycles;
   }
   else {
-    std::cerr << "Relaxing scrub interval from " << currentScrubIntervalCycles << " cycles to " << newInterval << " cycles\n";
+    std::cerr << "Relaxingggg scrub interval from " << currentScrubIntervalCycles << " cycles to " << newInterval << " cycles\n";
   }
   currentScrubIntervalCycles = newInterval;
 }
